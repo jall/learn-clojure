6902d3e2aed88b263f25b6aafdf3865aa030d3e4
Jon Hall <code@jall.me>
2017-07-22 16:09:03 +0100

    Add proto-repl as dependency

--- project.clj
+++ project.clj
@@ -3,7 +3,10 @@
   :url "https://github.com/jall/learn-clojure"
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [
+     [org.clojure/clojure "1.8.0"]
+     [proto-repl "0.3.1"]
+  ]
   :main ^:skip-aot learn-clojure.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
