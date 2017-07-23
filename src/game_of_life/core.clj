e4d7b6b95c9742e806b1dc36f6a777c023ccd723
Jon Hall <code@jall.me>
2017-07-23 19:42:27 +0100

    Got colours rendering correctly for alive cells

--- src/game_of_life/core.clj
+++ src/game_of_life/core.clj
@@ -8,18 +8,11 @@
 (def grid-size 50)
 (def grid-size-px (* grid-size square-size-px))
 
-(def dead-colour 240)
-(def alive-colour 50)
-
-(defn setup []
-    (q/frame-rate 30)
-    (random-grid grid-size))
-
-(defn update-state [state]
-    (identity state))
+(def dead-colour [90 90 90])
+(def alive-colour [240 240 240])
 
 (defn draw-square [x y is-alive]
-    ; (if is-alive (q/fill alive-colour) ((q/fill dead-colour)))
+    (apply q/fill (if is-alive alive-colour dead-colour))
     (q/rect
         (* x square-size-px)
         (* y square-size-px)
@@ -31,9 +24,17 @@
         :let [x (first coords) y (second coords)]]
         (draw-square x y is-alive)))
 
+(defn setup []
+    (q/frame-rate 30)
+    (random-grid grid-size))
+
+(defn update-state [state]
+    (identity state))
+
 (defn draw-state [state]
     ; Clear the sketch first.
-    (q/background dead-colour)
+    (apply q/background alive-colour)
+    (apply q/stroke alive-colour)
     (draw-grid state))
 
 (q/defsketch drawing
